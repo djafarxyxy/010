4959238e37c6755c036ac59262519d575a2837b2
djafarxyxy <farhanch12344@gmail.com>
2023-03-30 13:17:42 +0700

    display

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// array of intergers to hold values
+									// array of intergers to hold values
 int arr[20];
 int cmp_count = 0;																	// number of comparasion
 int mov_count = 0;																	// number of data movement
@@ -24,11 +24,11 @@ void input() {
 
 	for (int i = 0; i < n; i++)
 	{
-		cout << "<" << (1 + 1) << ">";
+		cout << "<" << (i + 1) << ">";
 		cin >> arr[i];
 	}
 }
-//Swaps the element at index x with the element at index y
+									//Swaps the element at index x with the element at index y
 void swap(int x, int y)
 {
 	int temp;
@@ -39,15 +39,15 @@ void swap(int x, int y)
 }
 
 
-void q_short(int low, int high)
+void q_sort(int low, int high)
 {
 	int pivot, i, j;																// Langkah 1
-	if (low > high)
+	if (low > high) {
 		return;
-
-	// partition the list into two part
-	// one containing element less that or equal to pivot
-	// outher containing elements greather than pivot
+	}
+									// partition the list into two part
+									// one containing element less that or equal to pivot
+									// outher containing elements greather than pivot
 
 	pivot = arr[low];																// Langkah 2
 
@@ -57,14 +57,14 @@ void q_short(int low, int high)
 
 	while (i <= j)																	// LAngkah 10
 	{
-		// seacrh for an element greather than pivot 
+									// seacrh for an element greather than pivot 
 		while ((arr[i] <= pivot) && (i <= high))									// Langkah 5
 		{
 			i++;																	// Langkah 6
 			cmp_count++;
 		}
 		cmp_count++;
-		// seacrh for an element less than or equal to pivot
+									// seacrh for an element less than or equal to pivot
 		while ((arr[j] > pivot) && (j >= low))										// Langkah 7
 		{
 			j--;																	// Langkah 8
@@ -72,26 +72,26 @@ void q_short(int low, int high)
 		}
 		cmp_count++;
 	}
-	// Langkah 9
-	if (i < j)	// if the greather element is on the left of the elemnt 
+																					// Langkah 9
+	if (i < j)						// if the greather element is on the left of the elemnt 
 	{
-		// swap the element at index i whith the element at index j
+									// swap the element at index i whith the element at index j
 		swap(i, j);
 		mov_count++;
 	}
-	// j now containt the index of the last element in the sorted list 
+									// j now containt the index of the last element in the sorted list 
 	if (low < j)																	// Langkah 11
 	{
-		// move the pivot to its correc position in the list
+									// move the pivot to its correc position in the list
 		swap(low, j);
 		mov_count++;
 	}
-	// sort the list on the left pivot using quick sort
-	q_short(low, j - i);															// Langkah 12
+									// sort the list on the left pivot using quick sort
+	q_sort(low, j - i);															// Langkah 12
 
 
-	// sort the list on the right of pivot using quick sort
-	q_short(j + 1, high);															// Langkah 13
+									// sort the list on the right of pivot using quick sort
+	q_sort(j + 1, high);															// Langkah 13
 
 
 }
@@ -116,8 +116,8 @@ int main()
 {
 
 	input();
-	// sort the array using quick sort
-	q_short(0, n - 1);
+									// sort the array using quick sort
+	q_sort(0, n - 1);
 	display();
 	system("pause");
 
